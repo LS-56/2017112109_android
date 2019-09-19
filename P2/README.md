ff58144750dafb1ecdc93114b15a264d9fcdb91b
ling_shang56 <1204741923@qq.com>
2019-09-19 18:07:55 +0800

    no message

---
+++ P2/README.md
@@ -0,0 +1,14 @@
+安卓作业二
+==========
+*在github上下载郭霖的代码导入Android Studio<br>
+*对 build.gradle做出以下修改：<br>
+1.更改gradle版本;<br>
+2.添加goole();<br>
+![image](https://github.com/LS-56/2017112109_android/blob/master/P2/images/1.png)<br><br>
+*对gradle-wrapper.properties文件做出以下修改：<br>
+![image](https://github.com/LS-56/2017112109_android/blob/master/P2/images/2.png)<br><br>
+*点击运行，结果如下图：<br>
+![image](https://github.com/LS-56/2017112109_android/blob/master/P2/images/3.png)<br><br>
+问题：一开始运行一直显示Emulator: emulator: ERROR: can't find the emulator executable<br>
+解决：更新了SDK<br>
+总结：对各个软件都还不够熟悉
---
+++ P3/HelloWorld/app/src/main/java/com/example/helloworld/Hello2.java
@@ -0,0 +1,81 @@
+package com.example.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Hello2 extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "Hello2";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello2);
+        Log.d(TAG, "onCreate execute");
+        settupClicks();
+        setTitle("Hello2");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart execute");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume execute");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause execute");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop execute");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy execute");
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button b;
+        Intent intent;
+        if(v.getId()==R.id.btToHello1){
+            intent=new Intent(Hello2.this,Hello1.class);
+            startActivity(intent);
+        }
+        if(v.getId()==R.id.btToHello2){
+            intent=new Intent(Hello2.this,Hello2.class);
+            startActivity(intent);
+        }
+        if(v.getId()==R.id.btToHello3){
+            intent=new Intent(Hello2.this,Hello3.class);
+            startActivity(intent);
+        }
+    }
+    private void settupClicks(){
+        Button b;
+        b=(Button)findViewById(R.id.btToHello1);
+        b.setOnClickListener(this);
+        b=(Button)findViewById(R.id.btToHello2);
+        b.setOnClickListener(this);
+        b=(Button)findViewById(R.id.btToHello3);
+        b.setOnClickListener(this);
+    }
+}
