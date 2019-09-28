f237ec56e25a57b8d1ec44139719c2aa7bde90e6
ling_shang56 <1204741923@qq.com>
2019-09-28 10:57:36 +0800

    standard

--- P4/HelloWorld/app/src/main/java/com/example/helloworld/Hello2.java
+++ P4/HelloWorld/app/src/main/java/com/example/helloworld/Hello2.java
@@ -1,22 +1,26 @@
 package com.example.helloworld;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 
-public class Hello2 extends AppCompatActivity implements View.OnClickListener{
+public class Hello2 extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "Hello2";
+    private static int objCount = 0;
+    private int mObjCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello2);
-        Log.d(TAG, "onCreate execute");
+        objCount++;
+        mObjCount = objCount;
+        Log.d(TAG, mObjCount + "-onCreate execute");
         settupClicks();
         setTitle("Hello2");
     }
@@ -24,14 +28,17 @@ public class Hello2 extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart execute");
+        Log.d(TAG,
+                mObjCount = objCount; "-onStart execute");
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume execute");
+        Log.d(TAG, objCount++;
+        mObjCount = objCount;
+        "-onResume execute");
     }
 
     @Override
@@ -56,26 +63,27 @@ public class Hello2 extends AppCompatActivity implements View.OnClickListener{
     public void onClick(View v) {
         Button b;
         Intent intent;
-        if(v.getId()==R.id.btToHello1){
-            intent=new Intent(Hello2.this,Hello1.class);
+        if (v.getId() == R.id.btToHello1) {
+            intent = new Intent(Hello2.this, Hello1.class);
             startActivity(intent);
         }
-        if(v.getId()==R.id.btToHello2){
-            intent=new Intent(Hello2.this,Hello2.class);
+        if (v.getId() == R.id.btToHello2) {
+            intent = new Intent(Hello2.this, Hello2.class);
             startActivity(intent);
         }
-        if(v.getId()==R.id.btToHello3){
-            intent=new Intent(Hello2.this,Hello3.class);
+        if (v.getId() == R.id.btToHello3) {
+            intent = new Intent(Hello2.this, Hello3.class);
             startActivity(intent);
         }
     }
-    private void settupClicks(){
+
+    private void settupClicks() {
         Button b;
-        b=(Button)findViewById(R.id.btToHello1);
+        b = (Button) findViewById(R.id.btToHello1);
         b.setOnClickListener(this);
-        b=(Button)findViewById(R.id.btToHello2);
+        b = (Button) findViewById(R.id.btToHello2);
         b.setOnClickListener(this);
-        b=(Button)findViewById(R.id.btToHello3);
+        b = (Button) findViewById(R.id.btToHello3);
         b.setOnClickListener(this);
     }
 }
